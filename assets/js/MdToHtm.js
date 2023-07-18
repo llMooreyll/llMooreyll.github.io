d98c9600a5a537720995946244eb29ee8b3c8537
llMooreyll <98308809+llMooreyll@users.noreply.github.com>
2023-07-18 19:18:11 +0800

    Add files via upload

---
+++ assets/js/MdToHtm.js
@@ -0,0 +1,78 @@
+{
+    let MarkdownIt = window.markdownit({
+        html: true,
+        linkify: true,
+        typographer: true,
+    });
+
+    let text = document.querySelector('#raw-markdown-body').textContent,
+        target = document.querySelector('#raw-markdown-body'),
+        result = MarkdownIt.render(text);
+
+    target.innerHTML = result;
+}
+
+{
+    let Index = 0;
+    let titleTag = ['H1', 'H2', 'H3'];
+    let titles = [];
+    let nodeToBeTraversed = document.querySelector('#raw-markdown-body');
+
+    function nodeTraverse(e, f) {
+        // let Layer = 'layer-' + Index;
+        f(e);
+        if (e.hasChildNodes()) {
+            Index += 1;
+            let children = e.childNodes;
+            for (let i = 0; i < children.length; i++) {
+                nodeTraverse(children[i], f);
+            }
+            Index -= 1;
+        }
+    }
+
+    // function generateIndex(e, layer) {
+    function generateIndex(e) {
+        // if (titleTag.includes(e.nodeName) && (layer == 'layer-1' || layer == 'layer-2')) {
+        if (titleTag.includes(e.nodeName)) {
+            // e.setAttribute('layer', layer);
+            let result = e.textContent;
+            e.setAttribute('id', result);
+            // let eChildOne = e.firstElementChild;
+            // for(let p = 0; p < e.childNodes.length; p++) {
+            //     result = strSubtract(result, eChildren[p]);
+            // }
+            // if (eChildOne != null) {
+            //     result = strSubtract(result, eChildOne.textContent);
+            // }
+            // console.log(result);
+            titles.push({
+                id: result,
+                // level: layer.substring(1),
+                level: e.nodeName.substring(1),
+                title: result,
+                // nodeName: e.nodeName,
+            });
+        }
+    }
+
+    nodeTraverse(nodeToBeTraversed, generateIndex);
+
+    // document.querySelector('#category').innerHTML +=
+    //     "<li class='index' style='font-size: 30px; padding-left: " + (titles[0].level * 22 - 22) + "px;'>" + 'Category' + '</li>';
+    for (let index in titles) {
+        document.querySelector('#category').innerHTML +=
+            "<li class='index' style='padding-left: " +
+            (titles[index].level * 22) +
+            "px;'>" +
+            "<a href='#" +
+            titles[index].id +
+            "'>" +
+            titles[index].title +
+            '</a></li>';
+    }
+
+    // function strSubtract(str1, str2) {
+    //     return str1.substring(0, str1.length - str2.length);
+    // }
+}
--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,21 +1,38 @@
 // Parallax effect lags on mobile, so we disable it based on the window resolution.
+let previousPageYOffset = 0;
 const parallaxImage = document.querySelector('#parallax-img');
+const category = document.querySelector('#category');
+const hiddenNavbar = document.querySelector('.fixed-top');
 const parallaxMinWidth = 768;
 const parallaxSpeed = 0.8;
 
 const parallaxTick = () => {
     if (window.innerWidth > parallaxMinWidth) {
-        // parallaxImage.style.transform = `translateY(${window.pageYOffset * parallaxSpeed}px)`;
         const distance = window.pageYOffset * parallaxSpeed;
         parallaxImage.style.transform = `translate3d(0, ${distance}px, 0)`;
-
     } else {
         parallaxImage.style.transform = 'none';
     }
 };
 
+function hideNavbar() {
+    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    if (scrollTop > 300) {
+        category.style.opacity = 1.0;
+    }else{
+        category.style.opacity = 0;
+    }
+    if (scrollTop - previousPageYOffset > 0) {
+        hiddenNavbar.style.top = `-70px`;
+    } else {
+        hiddenNavbar.style.top = `0px`;
+    }
+    previousPageYOffset = scrollTop;
+}
+
 window.addEventListener('scroll', () => {
     window.requestAnimationFrame(parallaxTick);
+    hideNavbar();
 });
 
 window.addEventListener('resize', () => {
